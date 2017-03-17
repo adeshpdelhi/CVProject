45210a6bbbbec691afcc869070e4bb33cc6105ae
Adesh Pandey <pandeyadeshdelhi@gmail.com>
2017-03-18 01:03:28 +0530

    implemented motion class detection (slow)

--- main.cpp
+++ main.cpp
@@ -1,19 +1,110 @@
+//Obtained from lkdemo.cpp in opencv samples
 #include <iostream>
 #include <ctype.h>
 #include <time.h>
+#include <set>
+#include <algorithm>
 #include "opencv2/video/tracking.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include <opencv2/opencv.hpp>
 #include <opencv2/nonfree/nonfree.hpp>
 #include "opencv2/highgui/highgui.hpp"
+#define PI 3.14159265
+
 using namespace cv;
 using namespace std;
 
-bool REMOVE_UNTRACKED_POINTS = false;
+class motion_vector{
+public:
+	Point2f point;
+	float angle;	//angle in degrees
+	motion_vector(Point2f point, float angle){
+		this->point = point; this->angle = acos(angle)*180/PI;
+	};
+};
+
+struct points_compare {
+    bool operator() (const Point2f& lhs, const Point2f& rhs) const{
+        if(lhs.x < rhs.x) return true;
+        if(rhs.x > lhs.x) return false;
+        if(lhs.y < rhs.y) return true;
+        if(rhs.y > lhs.y) return false;
+        return false;
+    }
+};
+
+bool REMOVE_UNTRACKED_POINTS = true;
+int PIXEL_WINDOW_FOR_MOTION_CLASS_ESTIMATION = 30;
+float ANGULAR_THRESHOLD_FOR_MOTION_CLASS_ESTIMATION = 20.0f;
+float THRESHOLD_FOR_BACKGROUND_FOREGROUND_DISTINCTION = 3.0f;
+int MINIMUM_NUMBER_OF_POINTS_IN_CLUSTER = 100;
 vector<Point2f> temp_points[2];
 vector<Point2f> points[2];
 vector <bool> foreground;
 vector<Point2f> new_points;
+vector<motion_vector> foreground_motion_vectors;
+vector< set<Point2f, points_compare> > cluster_foreground_vectors;
+
+bool compare_motion_vectors(const motion_vector m1, const motion_vector m2){
+	return m1.angle < m2.angle;
+}
+
+// friend Point2f operator<(const T& type, const Matrix& matrix)
+// {
+//     return matrix + type;
+// }
+
+// template <>
+// bool Point2f::operator<(const Point2f& rhs) const
+// {
+//    // return (rhs.x == this->x && rhs.y == this->y);
+// 	return false;
+// }
+
+
+
+void merge_points(Point2f p1, Point2f p2){
+	int s1 = -1;
+	for(int i = 0; i<cluster_foreground_vectors.size();i++)
+		if(cluster_foreground_vectors[i].find(p1) != cluster_foreground_vectors[i].end()){
+			s1 = i; break;
+		}
+	int s2 = -1;
+	for(int i = 0; i<cluster_foreground_vectors.size();i++)
+		if(cluster_foreground_vectors[i].find(p2) != cluster_foreground_vectors[i].end()){
+			s2 = i; break;
+		}
+	if(s1 == s2){
+		if(s1== -1) {
+			// create new cluster and add both
+			set<Point2f, points_compare> s;
+			s.insert(p1);s.insert(p2);
+			cluster_foreground_vectors.push_back(s);
+			return;
+		}
+		else //already in same cluster
+			return;
+	}
+	if(s1 != -1 && s2 == -1){
+		cluster_foreground_vectors[s1].insert(p2);
+	}
+	else if(s1 == -1 && s2 != -1){
+		cluster_foreground_vectors[s2].insert(p1);
+	}
+	else{
+		set <Point2f, points_compare> set1 (cluster_foreground_vectors[s1]);
+		set <Point2f, points_compare> set2 (cluster_foreground_vectors[s2]);
+		cluster_foreground_vectors.erase(cluster_foreground_vectors.begin() + s1);
+		cluster_foreground_vectors.erase(cluster_foreground_vectors.begin() + s2);
+		set <Point2f, points_compare> set_final;
+		for(set<Point2f, points_compare>::iterator i = set1.begin(); i!= set1.end();i++)
+			set_final.insert(*i);
+		for(set<Point2f, points_compare>::iterator i = set2.begin(); i!= set2.end();i++)
+			set_final.insert(*i);
+		cluster_foreground_vectors.push_back(set_final);
+	}
+
+}
 
 int main( int argc, char** argv )
 {
@@ -35,14 +126,21 @@ int main( int argc, char** argv )
     namedWindow( "Output", 1 );
     Mat gray, prevGray, image;
     int step = 20; // 10 pixels spacing between kp's
-    time_t t1 = time(0) ;
+    time_t t1 = time(0), t2 ;
     srand (time(NULL));
-    time_t r1 = time(0),r2;
+    Mat frame; 
+	Mat_ <float> H;
+    Size reduced_size(640,480);
+    int offsetx;
+    int offsety;
+    int SAMPLE_EVERY_N_FRAMES = 3;
+    int counter = 0;
     for(;;)
     {
-        Mat frame;
+    	// counter++;
+    	// if(counter%SAMPLE_EVERY_N_FRAMES != 0)
+    	// 	continue;
         cap >> frame;
-        Size reduced_size(640,480);
         cv::resize(frame, frame, reduced_size);
         if( frame.empty() )
             break;
@@ -52,15 +150,12 @@ int main( int argc, char** argv )
             image = Scalar::all(0);
         if( needToInit )
         {
-            int offsetx;
-            int offsety;
             points[1].clear();
             for (int y=step; y<frame.rows-step; y+=step){
 		        for (int x=step; x<frame.cols-step; x+=step){
 		        	offsetx =rand()%10;
 		        	offsety = rand()%10;
-		            Point2f point = Point2f((float)x+offsetx, (float)y+offsety);
-		            points[1].push_back(point);
+		            points[1].push_back(Point2f((float)x+offsetx, (float)y+offsety));
 		        }
 		    }
             needToInit = false;
@@ -88,31 +183,69 @@ int main( int argc, char** argv )
 				points[0].insert(points[0].end(), temp_points[0].begin(), temp_points[0].end());
 				points[1].insert(points[1].end(), temp_points[1].begin(), temp_points[1].end());
 			}
-		  	Mat_ <float> H = findHomography( points[0], points[1], CV_RANSAC );
+			try{
+		  		H = findHomography( points[0], points[1], CV_RANSAC );
+			}
+		  	catch(Exception e){
+		  		cout<<"Error! Insufficient number of points for findHomography. Skipping frame.\n";
+		  		continue;
+		  	}
+		  	// foreground_points.clear();
+		  	// foreground_motion_vectors.clear();
             for (int i = 0; i < points[0].size(); ++i)
             {
             	if(status[i] == 0){
-            		foreground.push_back(false);
+            		// foreground.push_back(false);
             		continue;
             	}
 	            Vec3f old_point = Vec3f((float)points[0][i].x, (float)points[0][i].y, 1.0);
                 Mat new_point_matrix  = H*Mat(old_point);
                 Point2f new_point = Point2f(new_point_matrix.at<float>(0,0)/new_point_matrix.at<float>(2,0),new_point_matrix.at<float>(1,0)/new_point_matrix.at<float>(2,0));
-
-                if(norm(points[1][i] - new_point)>2)
-                    foreground.push_back(true);
-                else
-                    foreground.push_back(false);
+                // if(norm(points[1][i] - new_point)>2)
+                //     foreground.push_back(true);
+                // else
+                //     foreground.push_back(false);
+                if(norm(points[1][i] - new_point)>THRESHOLD_FOR_BACKGROUND_FOREGROUND_DISTINCTION){
+                	//foreground point
+                	float cos_angle = (points[1][i].x - points[0][i].x)/(float)norm(points[1][i] - points[0][i]);
+                	motion_vector mVector(points[1][i], cos_angle);
+                	foreground_motion_vectors.push_back(mVector);
+                }
             }
-            for(int  i = 0; i < points[1].size(); i++ )
+            // sort(foreground_motion_vectors.begin(), foreground_motion_vectors.end(), compare_motion_vectors);
+            cluster_foreground_vectors.clear();
+            for (int i = 0; i < foreground_motion_vectors.size() ; ++i)
             {
-                if(foreground[i])
-                    circle( image, points[1][i], 3, Scalar(0,0,255), -1, 8);
-                else
-                    circle( image, points[1][i], 3, Scalar(0,255,0), -1, 8);
+            	for(int j = i+1; j<foreground_motion_vectors.size();j++){
+            		if(norm(foreground_motion_vectors[i].point - foreground_motion_vectors[j].point) < PIXEL_WINDOW_FOR_MOTION_CLASS_ESTIMATION && abs(foreground_motion_vectors[i].angle - foreground_motion_vectors[j].angle)<ANGULAR_THRESHOLD_FOR_MOTION_CLASS_ESTIMATION){
+            			merge_points(foreground_motion_vectors[i].point, foreground_motion_vectors[j].point);
+            		}
+            	}
             }
+            
+    		srand(time(NULL));	  int r1,r2,r3;  
+    		bool flag_atleast_one_cluster = false;    
+            for(int i = 0; i<cluster_foreground_vectors.size();i++){
+        		r1 = rand()%255; r2 = rand()%255; r3 = rand()%255;
+        		if(cluster_foreground_vectors[i].size()>MINIMUM_NUMBER_OF_POINTS_IN_CLUSTER){
+        			flag_atleast_one_cluster = true;
+        			cout<<"Cluster size"<<cluster_foreground_vectors[i].size()<<endl;
+	            	for(set<Point2f, points_compare> :: iterator j = cluster_foreground_vectors[i].begin(); j!= cluster_foreground_vectors[i].end();j++){
+	            		circle( image, *j, 3, Scalar(r1,r2,r3), -1, 8);
+        			}
+            	}
+            }
+            if(flag_atleast_one_cluster)
+            	cout<<"-----------\n";
+            // for(int  i = 0; i < points[1].size(); i++ )
+            // {
+            //     if(foreground[i])
+            //         circle( image, points[1][i], 3, Scalar(0,0,255), -1, 8);
+            //     else
+            //         circle( image, points[1][i], 3, Scalar(0,255,0), -1, 8);
+            // }
         }
-        needToInit = false;
+
         if(argc>=2){
 	        transpose(image, image);
 	        flip(image, image , 1);
@@ -138,10 +271,11 @@ int main( int argc, char** argv )
         }
         std::swap(points[1], points[0]);
         cv::swap(prevGray, gray);
-        time_t t2 = time(0) ;
+        t2 = time(0) ;
         if(difftime(t2,t1)>1){
         	t1 = time(0);
-	        needToInit = true;          
+	        needToInit = true;     
+	        foreground_motion_vectors.clear();     
         }
     }
     return 0;
